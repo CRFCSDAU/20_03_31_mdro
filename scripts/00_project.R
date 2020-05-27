debd1c049f50ca54fc7130e971b0bb2579554c93
DarrenDahly <darrendl1@gmail.com>
2020-05-27 19:03:35 +0100

    initialize project

---
+++ scripts/00_project.R
@@ -0,0 +1,71 @@
+# Project
+
+# Hi Darren,
+#
+# Thanks for taking the call on Friday. As promised here is the email outlining
+# the study.
+#
+# I have attached a word document I use to template/organise my thoughts for any
+# given study, and further detail is available in that.
+#
+# Suffice it to say however, we are interested in hip fracture patients who are
+# know carriers but not necessarily infected with a multi-drug resistant
+# organism (MDRO).
+#
+# Examples of these MDROs include MRSA/VRE etc.
+#
+#
+# In order to prevent other patients from being colonised/cross contaminated by
+# such organisms “MDRO patients” are isolated within the hospital i.e. put into
+# private rooms, bypass theatre reception, not brought to general patient
+# recovery area post op.
+#
+# While in theory this sounds fine, it actually really limits this cohort of
+# patients’ access to basic care. Anecdotally they wait for much longer to get a
+# bed on the ward and are left on trollies in ED far longer and also have to
+# wait much longer to get to theatre for their broken hip…..but nobody has shown
+# this.
+#
+# We have gotten hold of the national hip fracture data (which is a
+# comprehensive, prospectively collected data set). It’s time to drive on with
+# the study.
+#
+#
+# Primary outcomes: Time to theatre for hip fracture Fixation, Time to ward bed
+# from emergency department
+#
+# Secondary outcomes: Thirty day mortality, local cost analysis (we have more
+# detailed data for waterford)
+#
+#
+#
+# In addition to my word document I have attached the data file.
+#
+# I have done some work on it to generate 3 extra tabs:
+#
+# The MDRO tab with no-anticoagulants is the tab we are most interested in. If
+# someone is on blood thinners in the community, their hip fracture surgery is
+# often delayed until the effect of these bloods thinners is either worn off or
+# reversed. This is an obvious confounding factor, I have excluded them from the
+# rest of the MDRO patients in this tab.
+#
+# The second extra tab is where I dumped the details of the anti-coagulated MDRO
+# hips (i.e. MDRO hips on blood thinners in the community).
+#
+# The third tab is waterford hip fractures….these will hopefully form part of a
+# secondary cost analysis depending how the study goes.
+#
+#
+#
+# Finally, I have attached 3 other papers which have run similar analysis to
+# give you an idea of what we are after.
+#
+#
+# I’m really looking forward to working with you on this. I’m excited by this
+# project as I think it will arm us with information that will allow us to
+# improve patient care.
+#
+# Best,
+#
+# Tristan
+# 
